a6403ebccd556d51e8085d92604019e0b806e405
Baran <baranguzel2000@gmail.com>
2023-11-26 17:37:33 +0300

    Cpp04 is in progress

---
+++ Cpp04/ex01/Brain.cpp
@@ -0,0 +1,44 @@
+#include "Brain.hpp"
+
+Brain::Brain()
+{
+	std::cout << "Brain default constructor called" << std::endl;
+    this->ideas = new std::string[100];
+    for(int i = 0; i < 100; i++)
+    {
+        this->ideas[i] = "There is no idea";
+    }
+}
+
+Brain::Brain(std::string *idea) {
+	this->ideas = new std::string[100];
+	for (int i = 0; i < 100; i++)
+    {
+		this->ideas[i] = idea[i];
+    }
+}
+
+Brain::~Brain()
+{
+    delete [] this->ideas;
+	std::cout << "Brain destructor called" << std::endl;
+}
+
+Brain::Brain(const Brain& brain)
+{
+	std::cout << "Brain copy constructor called" << std::endl;
+	*this = brain;
+}
+
+Brain& Brain::operator=(const Brain& brain)
+{
+	std::cout << "Brain '=' operator called" << std::endl;
+	for(int i = 0; i < 100; i++)
+		this->ideas[i] = brain.ideas[i];
+	return (*this);
+}
+
+std::string* Brain::getIdeas(void)
+{
+	return (this->ideas);
+}
